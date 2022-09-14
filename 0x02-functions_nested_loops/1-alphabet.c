52e1615ef149743e8528d98167deea0ff57a17d4
Blessing-C <chikaodili07@gmail.com>
2022-09-14 07:08:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lower case
+ * Return: Always 0
+ */
+
+void  print_alphabet(void)
+{
+	char c;
+
+	while (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
