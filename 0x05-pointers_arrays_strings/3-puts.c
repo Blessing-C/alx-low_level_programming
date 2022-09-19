59f97a358c8fa96b7005cb2f927168abc90382f2
Blessing-C <chikaodili07@gmail.com>
2022-09-19 16:35:46 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _strlen - Prints a string
+  * @s: The string to print
+  *
+  * Return: Returns length of the string
+  */
+int _strlen(char *s)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
