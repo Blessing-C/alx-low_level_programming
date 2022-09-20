f1a662e66d6aa609e17c9143dbf1d57ed84c7da7
Blessing-C <chikaodili07@gmail.com>
2022-09-19 22:41:20 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,12 +3,13 @@
 /**
   * print_array - print n elements of an array of integers
   * @a: array of integers
-  * @n: number of elements input   
+  * @n: number of elements input
   *   Return: void
   */
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; ++i)
 	{
 		if (i != (n - 1))
