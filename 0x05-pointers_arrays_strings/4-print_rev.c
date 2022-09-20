bafbee713a9c3d1eb02b8738a8c865fd44148321
Blessing-C <chikaodili07@gmail.com>
2022-09-19 21:47:52 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-  * print_rev - Prints a string in reverse
+  * print_rev: - Prints a string in reverse
   * @s: The string to print
   *
   * Return: void
@@ -10,7 +10,7 @@ void print_rev(char *s)
 {
 	int c = o;
 
-	while (s[c] != '\0')
+	while (s[c] != '\0');
 	{
 	c++;
 	}
