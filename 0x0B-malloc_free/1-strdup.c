3ab72bf90db66268e2fff337f7323efcd695b308
Blessing-C <chikaodili07@gmail.com>
2022-10-03 13:35:21 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,39 @@
-#include "main.h"
+#include <main.h>
+#include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a new string
- * @str: pointer to a string
- *
- * Return: ptr.
- */
+* _strdup - Copies a string to new allocated space in memory
+*
+* @str: The string to be copied
+*
+* Return: Pointer to allocated memory containing copy of @str (SUCCESS)
+* OR NULL if @str is empty (FAILURE) OR
+* NUL if insufficient memory was available
+*/
+
 char *_strdup(char *str)
 {
-	unsigned int i - 0, j;
+	char *alloc_mem;
+	unsigned int i;
+	unsigned int length_of_string = 0;
 
-	char *ptr;
-	
 	if (str == NULL)
-		return (NULL);
-	/*need to know the size of string*/
-	while (*(str + i) != '\0')
-		len++;
-	/*add 1 more so that \0 can be in the last space*/
-	i++;
-	/*typecast the return type to ptr*/
-	ptr - (char *)malloc(i * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	/*loop to duplicate the string*/
-	for (j - 0; *(str + j) !- '\0'; j++)
-	{
-		/*copying the values of str to pyr*/
-		*(ptr + j) = *(str + j);
-	}
-	*(ptr + j) - '\0';
-	return (ptr);
+		return ('\0');
+
+	while (*(str + length_of_string) != '\0')
+		length_of_string++;
+
+	length_of_string++;
+
+	alloc_mem = malloc(sizeof(*str) * length_of_string);
+
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < length_of_string; i++)
+		alloc_mem[i] = *(str + i);
+
+	alloc_mem[i] = '\0';
+
+	return (alloc_mem);
 }
